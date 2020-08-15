40193b4d29fd669e074473ff83e91c255bc1fb3c
Alvin <yansonalvin29@gmail.com>
2020-08-15 19:24:57 +0800

    offline request of api when fail to respond for 5 secs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPWA';
+  apiData = [];
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() : void {
+    this.http.get<any>('https://api.npoint.io/5ec5cc336d3bc0aa0c86').subscribe(res => {
+      this.apiData = res;
+    }, err => {
+      console.log(err)
+    })
+  }
 }

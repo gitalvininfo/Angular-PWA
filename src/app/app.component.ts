fe239ad77166f693988eb3b8166d76e46bbe4824
Alvin <yansonalvin29@gmail.com>
2020-08-16 12:05:51 +0800

    swupdate for angular - check for incoming updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ApplicationRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,15 +10,48 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'AngularPWA';
   apiData = [];
-  constructor(private http: HttpClient) {
-
+  constructor(private http: HttpClient, private update: SwUpdate, private appRef: ApplicationRef) {
+    this.updateClient();
+    this.checkUpdate();
   }
 
   ngOnInit() : void {
-    this.http.get<any>('https://api.npoint.io/5ec5cc336d3bc0aa0c86').subscribe(res => {
+    this.http.get<any>('https://api.npoint.io/f67fede347e7fc80093c').subscribe(res => {
       this.apiData = res;
     }, err => {
       console.log(err)
+    })
+  }
+
+  updateClient() {
+    if(!this.update.isEnabled) {
+      console.log('not enabled');
+      return;
+    }
+
+    this.update.available.subscribe((event) => {
+      console.log(`current`, event.current, `available`, event.available);
+        if(confirm('update available for the app please confirm...')) {
+          this.update.activateUpdate().then(() => location.reload());
+        }
+    })
+
+    this.update.activated.subscribe((event) => {
+      console.log(`current`, event.previous, `available`, event.current)
+
+    })
+  }
+
+  checkUpdate() {
+    this.appRef.isStable.subscribe((isStable) => {
+      if(isStable) {
+        const timeInterval = interval(6000)
+
+        timeInterval.subscribe(() => {
+          this.update.checkForUpdate().then(() => console.log('checked'));
+          console.log('update checked.')
+        })
+      }
     })
   }
 }
